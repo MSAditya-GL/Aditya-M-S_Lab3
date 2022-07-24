962c68bd01ceb499c72e167530df1a5dfe569d6a
Aditya <adigowdtham@gmail.com>
2022-07-25 00:26:56 +0530

    Inserted the empty lines as per the problem statement.

--- Question1/src/balanced/brackets/main/DriverClass.java
+++ Question1/src/balanced/brackets/main/DriverClass.java
@@ -8,9 +8,9 @@ public class DriverClass {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		BalancedString obj = new BalancedString();
-		System.out.println("Enter the option 1 or 2"+ '\n');
+		System.out.println("Enter the option 1 or 2" + '\n');
 		System.out.println("Option 1 : To check the String");
-		System.out.println("Option 2 : Demo"+ '\n');
+		System.out.println("Option 2 : Demo" + '\n');
 		int opt = sc.nextInt();
 		switch (opt) {
 		case 1: {
@@ -27,11 +27,11 @@ public class DriverClass {
 
 		case 2: {
 			String demo = "( [ [ { } ] ] )";
-			System.out.println("Sample Input : "+ '\n');
-			System.out.println(demo+ '\n');
+			System.out.println("Sample Input : " + '\n');
+			System.out.println(demo + '\n');
 
 			if (obj.check(demo)) {
-				System.out.println("Sample Output"+ '\n');
+				System.out.println("Sample Output" + '\n');
 				System.out.println("The entered String has Balanced Brackets");
 			} else {
 				System.out.println("Sample Output" + '\n');
